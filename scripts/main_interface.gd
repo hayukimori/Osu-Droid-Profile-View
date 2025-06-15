79817f404c43fc59abc195e4df8db9778040043d
hayukimori <hayukimori@gmail.com>
2025-06-15 02:15:21 -0300

    update: added entire logic to profile_editor

--- scripts/main_interface.gd
+++ scripts/main_interface.gd
@@ -1,3 +1,9 @@
 extends Control
 
+
 @onready var profile_editor: Node = $ProfileEditor
+
+
+func _ready() -> void:
+	#profile_editor.update_profile()
+	pass
--- scripts/profile_editor.gd
+++ scripts/profile_editor.gd
@@ -1,4 +1,6 @@
 extends Node
+class_name ProfileEditor
+
 # Node task: Edit Profile Info
 
 
@@ -17,26 +19,108 @@ var old_avatar_request_api_url: String = "https://osudroid.moe/user/avatar/%d.pn
 @export var overall_score_btn: Button
 
 @export var country_flag_texture_rect: TextureRectRounded
+@export var rank_country_flag_texture_rect: TextureRectRounded
 @export var profile_picture_texture_rect: TextureRectRounded
 
 @export_category("Settings")
 @export var avatar_request_api_method: AVATAR_REQUEST_METHOD = AVATAR_REQUEST_METHOD.OLD
 @export_dir var flags_directory: String = "res://assets/textures/country_flags"
+@export_file("*.png", "*.jpg", "*.webp", "*.svg") var default_profile_picture: String
 
 @export_category("Post content")
 @export var player_data: Dictionary = {}
 
 
+class GeneralTools:
+	func detect_image_format(data: PackedByteArray) -> String:
+		if data.size() >= 9:
+			var png_header := PackedByteArray([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A])
+			if data.slice(0, 8) == png_header:
+				return "png"
+		if data.size() >= 2:
+			if data[0] == 0xFF and data[1] == 0xD8:
+				return "jpg"
+		if data.size() >= 12:
+			var riff_str = data.slice(0, 4).get_string_from_ascii()
+			var webp_str = data.slice(8, 12).get_string_from_ascii()
+			if riff_str == "RIFF" and webp_str == "WEBP":
+				return "webp"
+		return "unknown"
+	
+	func format_numbers_dots(number: int) -> String:
+		var str_num = str(number)
+		var result = ""
+		var counter = 0
+
+		for i in range(str_num.length() - 1, -1, -1):
+			result = str_num[i] + result
+			counter += 1
+			if counter %3== 0 and i != 0:
+				result = "." + result
+		return result
+
+
+
+# Create image tools
+var gen_tools: GeneralTools = GeneralTools.new()
+
+
+# => Update profile
 func update_profile() -> void:
 	get_user_pfp(player_data.UserId)
+	update_flags(player_data.Region)
+	set_username(player_data.Username)
+	set_rankings(player_data.CountryRank, player_data.GlobalRank)
+	set_overall_infos()
 
 
+
+# => Set buttons (play count, accuracy, pp, etc) content.
+func set_overall_infos() -> void:
+	play_count_btn.text = "%d" % player_data.OverallPlaycount
+	overall_score_btn.text = "%s" % gen_tools.format_numbers_dots(int(player_data.OverallScore))
+	accuracy_btn.text = "%.2f" % (player_data.OverallAccuracy * 100) + "%"
+
+	
+	if player_data.OverallPP == null:
+		pp_btn.text = "0.00"
+	else:
+		pp_btn.text = player_data.OverallPP
+
+
+# => Sets the player's rankings
+func set_rankings(country_rk: int, global_rk: int) -> void:
+	national_rank_label.text = "# %d" % country_rk
+	global_rank_label.text = "# %d" % global_rk
+
+
+# => Sets the player's username
+func set_username(username: String) -> void:
+	username_label.text = username
+
+
+# => Update country flag
+func update_flags(region: String) -> void:
+	var flag_path = flags_directory + "/%s.svg" % region
+
+	if ResourceLoader.exists(flag_path):
+		rank_country_flag_texture_rect.texture = load(flag_path)
+		country_flag_texture_rect.texture = load(flag_path)
+	else:
+		rank_country_flag_texture_rect.texture = null
+		country_flag_texture_rect.texture = null
+
+	log(flag_path)
+
+
+# => Gets User profile picture and send it to the profile picture (TextureRectRounded)
 func get_user_pfp(uid: int) -> void:
 	# New http request, add it as a child
 	var http_request = HTTPRequest.new()
 	add_child(http_request)
 
 	http_request.request_completed.connect(self._http_image_requester_request_completed)
+
 
 	# Verify current request method, and launches corresponding method
 	match AVATAR_REQUEST_METHOD:
@@ -54,7 +138,37 @@ func get_user_pfp(uid: int) -> void:
 				push_error("An error occurred in HTTP Request")
 		_:
 			push_error("Invalid AVATAR_REQUEST_METHOD")
-	
 
-func _http_image_requester_request_completed(result, response_code, _headers, body):
-	pass
+
+
+
+
+#######################################
+# Subfunctions
+#######################################
+func _http_image_requester_request_completed(result, _response_code, _headers, body):
+	if result != HTTPRequest.RESULT_SUCCESS:
+		push_error("Image couldn't be downloaded")
+	
+	var image = Image.new()
+	var format = gen_tools.detect_image_format(body)
+	var err = OK
+
+	match format:
+		"png":
+			err = image.load_png_from_buffer(body)
+		"jpg", "jpeg":
+			err = image.load_jpg_from_buffer(body)
+		"webp":
+			err = image.load_webp_from_buffer(body)
+		_:
+			push_error("Unsupported image format: %s" % format)
+			err = image.load(default_profile_picture)
+
+	if err != OK:
+		push_error("Couldn't load image. Error code: %d" % err)
+		return
+	else:
+		var texture = ImageTexture.create_from_image(image)
+		profile_picture_texture_rect.texture = texture
+
